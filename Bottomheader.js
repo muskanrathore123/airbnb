833663143fb94b4b1f196deb639bdb80e762a7e5
muskanrathore123 <137879695+muskanrathore123@users.noreply.github.com>
2023-12-24 18:00:06 +0530

    Add files via upload

--- Bottomheader.js
+++ Bottomheader.js
@@ -1,18 +1,72 @@
 import React from 'react'
-import DatasetIcon from "@mui/icons-material/Dataset";
-import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+import KeyboardArrowDownSharpIcon from "@mui/icons-material/KeyboardArrowDownSharp";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import RemoveOutlinedIcon from "@mui/icons-material/RemoveOutlined";
 
 function Bottomheader() {
   return (
-    <div>
-      <div>
-        <DatasetIcon />
-        <MapOutlinedIcon />
+    <div className="sticky top-0 border-b-2 border-gray-300 flex justify-between items-center h-24">
+      <div className="px-10 py-10 flex flex-none  ">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="white"
+          class="bg-black   h-14 w-14 rounded-lg p-3"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3.75 6A2.25 2.25 0 0 1 6 3.75h2.25A2.25 2.25 0 0 1 10.5 6v2.25a2.25 2.25 0 0 1-2.25 2.25H6a2.25 2.25 0 0 1-2.25-2.25V6ZM3.75 15.75A2.25 2.25 0 0 1 6 13.5h2.25a2.25 2.25 0 0 1 2.25 2.25V18a2.25 2.25 0 0 1-2.25 2.25H6A2.25 2.25 0 0 1 3.75 18v-2.25ZM13.5 6a2.25 2.25 0 0 1 2.25-2.25H18A2.25 2.25 0 0 1 20.25 6v2.25A2.25 2.25 0 0 1 18 10.5h-2.25a2.25 2.25 0 0 1-2.25-2.25V6ZM13.5 15.75a2.25 2.25 0 0 1 2.25-2.25H18a2.25 2.25 0 0 1 2.25 2.25V18A2.25 2.25 0 0 1 18 20.25h-2.25A2.25 2.25 0 0 1 13.5 18v-2.25Z"
+          />
+        </svg>
+
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="gray"
+          class=" bg-gray-200  h-14 w-14 rounded-lg p-3"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M9 6.75V15m6-6v8.25m.503 3.498 4.875-2.437c.381-.19.622-.58.622-1.006V4.82c0-.836-.88-1.38-1.628-1.006l-3.869 1.934c-.317.159-.69.159-1.006 0L9.503 3.252a1.125 1.125 0 0 0-1.006 0L3.622 5.689C3.24 5.88 3 6.27 3 6.695V19.18c0 .836.88 1.38 1.628 1.006l3.869-1.934c.317-.159.69-.159 1.006 0l4.994 2.497c.317.158.69.158 1.006 0Z"
+          />
+        </svg>
       </div>
-      <div>
-        <button>h1</button>
-        <button>h1</button>
-        <button>h1</button>
+      <div className="flex justify-envenly space-x-3 ml-12 mr-12 ">
+        <p className="bg-gray-200 w-80  px-5 py-5 rounded-lg -ml-72 text-xl">
+          Anywhere
+          <span className="ml-40  ">
+            <KeyboardArrowDownSharpIcon />
+          </span>
+        </p>
+        <p className="bg-gray-200  w-48 text-xl rounded-lg px-5 py-5">
+          <CalendarTodayOutlinedIcon /> <span>June 14-21</span>
+        </p>
+        <p className="bg-gray-200  w-48 text-xl rounded-lg px-5 py-5">
+          <AddOutlinedIcon /> <span>4 Guests</span><RemoveOutlinedIcon/>
+        </p>
+      </div>
+      <div className="px-10 py-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="gray"
+          className=" bg-gray-200 focus:outline-teal-500 h-14 w-14 rounded-lg p-3 "
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
+          />
+        </svg>
       </div>
     </div>
   );
